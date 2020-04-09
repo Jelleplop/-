8358ee8014c97c5c111178c48887e6c653fe5070
Jelleplop <Jelleplop@FX>
2020-04-09 10:01:21 +0400

    Добавьте файлы проекта.

---
+++ Динамические массивы/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dinamic 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<short> one = new List<short>() { 1, 0, 57, 97 };
+            one.Add(21);
+            one.AddRange(new short[] { 83, 53, 11});
+            one.Insert(1, 107);
+            one.Remove(0);
+            one.RemoveAt(2);
+            one.Sort();
+
+            Console.WriteLine(one[2]);
+
+
+
+        }
+    }
+}
